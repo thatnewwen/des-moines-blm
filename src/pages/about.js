21a7139b226abebbc2379dcfd04b165f9115d66a
theProtestant <chloefourte97@gmail.com>
2020-08-04 18:29:25 -0500

    Paragraph added to about page.

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,14 @@ const About = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>About</h1>
-    <h2>Our origin</h2>
+    <h2>Who Are We?</h2>
+    <p>
+    When organizers in DSM first convened, there was an immediate goal to create a Des Moines
+    Black Lives Matter chapter. After beginning actions and thinking further about the possibility of becoming a chapter, it was decided to not continue with that route and instead become an entity of our own. <br>
+    Des Moines Black Liberation Movement is a collective of young organizers in Des Moines,made up of various gender identities, cultural backgrounds, and political beliefs - but brought together around the unifying understanding that Black people matter and have been systematically oppressed since the inception of this nation. Des Moines BLM organizes to elevate awareness around the inner workings of systematic oppression in order to root them out. Des Moines BLM came together following the horrific murder of George Floyd by the Minneapolis Police Department, and are adamantly focused on abolishing police, starting with the Des Moines Police Department. <br>
+    Des Moines BLM was founded on June 2nd, 2020 and established their first community protest on June 3rd, 2020.<br>
+    We operate under two separate branches, the Des Moines Black Liberation Movement Foundation, a 501(c)(3), and the Des Moines Black Liberation Movement Collective, a 501(c)(4). <br>
+    </p>
     <h2>What we believe</h2>
     <h2>What we’re fighting for</h2>
     <h2>Our partners</h2>
